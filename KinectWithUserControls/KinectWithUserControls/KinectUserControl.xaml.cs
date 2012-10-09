e6eac9439a7f718f2b7b480d54bba184b49f77f8
Ewerton <ewerton.sc@gmail.com>
2012-10-08 22:45:34 -0300

    Creation
    I create the MainWindow and the UserControl.
    The user control references the WpfColorViewer provided in Kinect
    Samples.
    I used the KinectSensorChooser from the Kinect toolkit to discover the
    sensor.
    Needed to pass the reference of the discovered sensor to UserControl.

---
+++ KinectWithUserControls/KinectWithUserControls/KinectUserControl.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Samples.Kinect.WpfViewers;
+using Microsoft.Kinect;
+
+namespace KinectWithUserControls
+{
+    /// <summary>
+    /// Interaction logic for KinectUserControl.xaml
+    /// </summary>
+    public partial class KinectUserControl : UserControl
+    {
+        // the user control need to expose a KinectSensor property to be binded, right?
+        // i preffer to expose a KinectSensor than KinectSensorManager
+        public static readonly DependencyProperty KinectSensorProperty =
+            DependencyProperty.Register(
+                "KinectSensor",
+                typeof(KinectSensor),
+                typeof(KinectUserControl),
+                new PropertyMetadata(null));
+
+        public KinectSensor KinectSensor
+        {
+            get { return (KinectSensor)GetValue(KinectSensorProperty); }
+            set { SetValue(KinectSensorProperty, value); }
+        }
+
+
+
+        // The color viewer needs a KinectSensorManager bind, how to do this?
+
+        public KinectUserControl()
+        {           
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            // this type of attribuition is possible?
+            //ColorViewer.KinectSensorManager = someKinectSensorManager;
+
+            this.KinectSensor.ColorFrameReady += new EventHandler<ColorImageFrameReadyEventArgs>(KinectSensor_ColorFrameReady);
+            this.KinectSensor.AllFramesReady += new EventHandler<AllFramesReadyEventArgs>(KinectSensor_AllFramesReady);
+        }
+
+        void KinectSensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        void KinectSensor_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            // just to test is this breakpoint will be reached
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ KinectWithUserControls/KinectWithUserControls/MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect.Toolkit;
+using Microsoft.Samples.Kinect.WpfViewers;
+using Microsoft.Kinect;
+
+namespace KinectWithUserControls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Declare a SensorChooser from KinectToolkit and  and a SensorManager from Samples.Kinect.WpfViewers
+        // Is possíble to use only one ?
+        KinectSensorChooser SensorChooser = new KinectSensorChooser();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.SensorChooser.KinectChanged += new EventHandler<KinectChangedEventArgs>(SensorChooser_KinectChanged);
+
+            // Starts the Kinect discovering process.
+            this.SensorChooser.Start();
+        }
+
+        void SensorChooser_KinectChanged(object sender, KinectChangedEventArgs e)
+        {
+            KinectSensor oldSensor = (KinectSensor)e.OldSensor;
+            StopKinect(oldSensor);
+
+            KinectSensor newSensor = (KinectSensor)e.NewSensor;
+
+            if (newSensor == null)
+            {
+                // No sensor detected
+                return;
+            }
+
+            try
+            {
+               StartKinect(newSensor);
+            }
+            catch (System.IO.IOException)
+            {
+                this.SensorChooser.TryResolveConflict();
+            }
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            // i know, some validations is needed.
+            StartKinect(this.SensorChooser.Kinect);
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            StopKinect(this.SensorChooser.Kinect);
+        }
+
+        private void StopKinect(KinectSensor sensor)
+        {
+            if (sensor != null)
+            {
+                if (sensor.IsRunning)
+                {
+                    sensor.Stop();
+                   
+                    if (sensor.AudioSource != null)
+                    {
+                        sensor.AudioSource.Stop();
+                    }
+
+                    btnStart.IsEnabled = true;
+                    btnStop.IsEnabled = false;
+                    txtKinectStatus.Text = "Kinect Stopped";
+                }
+            }
+        }
+
+        private void StartKinect(KinectSensor sensor)
+        {
+            sensor.Start();
+
+            btnStart.IsEnabled = false;
+            btnStop.IsEnabled = true;
+            txtKinectStatus.Text = "Kinect Started";
+
+            //Where i need to write the code that sends the sensor information (color stream, sekeleton stream, etc) to the 
+            // user control? Here?
+            kinectUserControl1.KinectSensor = this.SensorChooser.Kinect;
+
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            //this.SensorChooser.Stop();
+        }
+
+
+
+    }
+}
